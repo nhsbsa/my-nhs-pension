e612f862a49cd6db21f2c640dc3046bc86aaff9b
Elaine Russell <elaine.russell4@nhs.net>
2022-12-22 16:02:56 +0000

    General progress upload

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,11 @@
 // ES6 or Vanilla JavaScript
+
+//Security question /live/registration/step-3
+function showHide() {
+    let securityqu1 = document.getElementById('security1')
+    if (securityqu1.value != "") {
+        document.getElementById('answer1').style.display = 'block'
+    } else {
+        document.getElementById('answer1').style.display = 'none'
+    }
+}
